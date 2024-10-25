c15e6244d87634a3ceb0c9f263ab17088e311c88
1xstj <106580853+1xstj@users.noreply.github.com>
2024-10-25 15:15:14 +0100

    build paseo release

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -74,7 +74,7 @@ pub fn development_config() -> ChainSpec {
 		Extensions {
 			relay_chain: "rococo-local".into(),
 			// You MUST set this to the correct network!
-			para_id: 1000,
+			para_id: 4535,
 		},
 	)
 	.with_name("Development")
